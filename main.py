efd69320c8957d330aacbe932f7802aacd31e359
최준서 <pororojs30@gmail.com>
2022-12-27 18:34:57 +0900

    login

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-id = ""
-pw = ""
+id = "test"
+pw = "test"
 
 # 에브리타임 로그인
 driver = webdriver.Chrome()
 driver.get("https://everytime.kr/login")
+driver.find_element(By.XPATH,"//input[@name='userid']").send_keys(id)
+driver.find_element(By.XPATH,"//input[@name='password']").send_keys(pw)
+driver.find_element(By.XPATH,"//input[@type='submit']").click()
+print()
 # 내가 쓴 글 페이지 들어가기
 
 # 상단 게시글 들어가기
